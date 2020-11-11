7146039b01c37527edb835ac1692945b21b93d55
Matt Gilboy <38512594+mattgilboy@users.noreply.github.com>
2020-11-11 12:14:15 -0500

    Initial Commit

---
+++ Shared/BasicTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicTipApp.swift
+//  Shared
+//
+//  Created by Matt Gilboy on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BasicTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
